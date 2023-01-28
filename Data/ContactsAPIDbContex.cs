d6de63d2587496838f595312db547c12dfe39ed4
gkemas <https://github.com/gkemas>
2023-01-29 01:36:54 +0300

    first commit

---
+++ Data/ContactsAPIDbContex.cs
@@ -0,0 +1,13 @@
+﻿using ContactsAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ContactsAPI.Data
+{
+    public class ContactsAPIDbContex : DbContext //ContactAPIDbContex e ctrl. ile alttaki options oluştu kısayol.
+    {
+        public ContactsAPIDbContex(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<Contact>Contacts { get; set; }
+    }
+}
